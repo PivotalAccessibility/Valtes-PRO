38bd211e9f3ae0af481fa2c216e0cf969d9c9bea
Prince <prince@pivotalaccessibility.com>
2025-02-27 13:38:43 +0530

    call global form, about, articles in content-partners

--- page-templates/content_partners.php
+++ page-templates/content_partners.php
@@ -14,9 +14,9 @@ get_header();
 <?php get_template_part('template-parts/content_partners/partners'); ?>
 <?php get_template_part('template-parts/content_partners/explanation'); ?>
 <?php get_template_part('template-parts/content_partners/clients'); ?>
-<?php get_template_part('template-parts/content_partners/form'); ?>
-<?php get_template_part('template-parts/content_partners/articles'); ?>
-<?php get_template_part('template-parts/content_partners/over-valtes', null, $explain); ?>
+<?php get_template_part('common-parts/global-form'); ?>
+<?php get_template_part('common-parts/global-bottom-articles', null, array('isDarkBg' => true)); ?>
+<?php get_template_part('common-parts/over-valtes'); ?>
 
 <?php
 
