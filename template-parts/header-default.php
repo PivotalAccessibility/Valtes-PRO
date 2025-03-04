e00c809b7e309fa0182616593d1f78160dcc849d
Sanyam <ajay@pivotalaccessibility.com>
2025-03-04 16:55:43 +0530

    Header fixed with updated version.

--- template-parts/header-default.php
+++ template-parts/header-default.php
@@ -4,6 +4,8 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
+$header = valtes_get_field('header', [], 'option');
+
 ?>
 
 <header id="header" x-data="header" role="banner" class=" bg-transparent fixed top-0 w-full !z-50">
@@ -11,15 +13,17 @@ if (!defined('ABSPATH')) {
     <section class="bg-[#1c233e] text-white">
         <div class="container flex items-center justify-between py-2">
             <div class="hidden sm:block">
-                Toegankelijkheidsinstellingen
+                <?php esc_html_e( "Toegankelijkheidsinstellingen" , "valtes" ) ?>
             </div>
             <div class="flex items-center justify-center w-full gap-8 sm:w-auto">
-                <a href="#" class="flex justify-between gap-4 underline">Lettertype
-                    <img src="<?php echo valtes_assets('images/goals-images/Down-Arrow.svg') ?>" alt="">
-                    <img src="<?php echo valtes_assets('images/goals-images/Up-Arrow.svg') ?>" alt="">
+                <a href="#" class="flex justify-between gap-4 underline">
+                    <?php esc_html_e( "Lettertype" , "valtes" ) ?>
+                    <img src="<?php echo valtes_assets('images/Subtract.svg') ?>" alt="">
+                    <img src="<?php echo valtes_assets('images/Add.svg') ?>" alt="">
                 </a>
-                <a href="#" class="flex justify-between gap-4 underline">vergroot contrast
-                    <img src="<?php echo valtes_assets('images/goals-images/contrast.svg') ?>" alt="">
+                <a href="#" class="flex justify-between gap-4 underline">
+                    <?php esc_html_e( "vergroot contrast" , "valtes" ) ?>
+                    <img src="<?php echo valtes_assets('images/CircleHalf.svg') ?>" alt="">
                 </a>
             </div>
         </div>
@@ -28,10 +32,10 @@ if (!defined('ABSPATH')) {
     <div class="container items-center justify-between hidden py-2 lg:flex">
         <div class="flex items-center ">
             <a href="<?php echo home_url(); ?>" class="">
-                <img src="<?php echo valtes_assets('images/Valtespro_logo.png') ?>" alt="" class="w-auto h-6 ">
+                <img src="<?php echo $header['valtespro_logo']['url'] ?>" alt="<?php echo $header['valtespro_logo']['alt'] ?>" class="w-auto h-6 ">
             </a>
 
-            <div class="ml-10 ">
+            <div class="ml-10">
                 <?php                
                 wp_nav_menu(array(
                     'theme_location' => 'my-custom-menu',
@@ -43,16 +47,18 @@ if (!defined('ABSPATH')) {
             </div>
         </div>
 
-        <a href="" class="flex items-center px-3 py-2 text-xs font-semibold text-blue-700 bg-white border-2 border-blue-700 rounded-full">
+        <a href=""
+            class="flex items-center px-3 py-2 text-xs font-semibold text-blue-700 bg-white border-2 border-blue-700 rounded-full">
             <span>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 mr-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 mr-2">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
                     <path d="M11 13l9 -9" />
                     <path d="M15 4h5v5" />
                 </svg>
             </span>
-            Voor professionals
+            <?php echo $header['cta']['title'] ?>
         </a>
 
     </div>
@@ -63,8 +69,9 @@ if (!defined('ABSPATH')) {
             <img src="<?php echo valtes_assets('images/Valtespro_logo.png') ?>" alt="" class="w-20 ">
         </a>
         <div>
-            <a href="" class="sm:flex items-center px-3 py-2 text-sm font-semibold text-primary bg-white border-2 border-primary rounded-full w-full">
-                Voor professionals
+            <a href=""
+                class="sm:flex items-center px-3 py-2 text-sm font-semibold text-primary bg-white border-2 border-primary rounded-full w-full">
+                <?php echo $header['cta']['title'] ?>
             </a>
         </div>
         <div class="px-3 py-4 bg-primary rounded-lg">
@@ -82,51 +89,48 @@ if (!defined('ABSPATH')) {
     <div id="mobileMenu" class="z-40 flex-col h-screen hidden p-6 bg-white">
         <div class="flex flex-col justify-between h-[80vh]">
             <div class=" mt-10">
-                <h2 class="text-xl font-bold text-center mb-4 text-[#1C233E]">Menu</h2>
+                <h2 class="text-xl font-bold text-center mb-4 text-[#1C233E]">
+                    <?php esc_html_e( "Menu" , "valtes" ) ?>
+                </h2>
                 <?php
-                //   wp_nav_menu( 
-                //     array( 
-                //         'theme_location' => 'my-custom-menu'
-                //     ) 
-                // ); 
-                
-                wp_nav_menu(array(
-                    'theme_location' => 'my-custom-menu',
-                    'container' => 'nav',
-                    'menu_class' => 'navbar-menu',
-                    // 'walker' => new Custom_Nav_Walker()
-                ));
+                    wp_nav_menu(array(
+                        'theme_location' => 'my-custom-menu',
+                        'container' => 'nav',
+                        'menu_class' => 'navbar-menu',
+                        // 'walker' => new Custom_Nav_Walker()
+                    ));
                 ?>
             </div>
             <div class="">
-                <button class="py-4 font-bold text-center text-white bg-primary text-xl rounded-full w-full">Download
-                    de Valtes app</button>
+                <button class="py-4 font-bold text-center text-white bg-primary text-xl rounded-full w-full">
+                    <?php esc_html_e( "Download de Valtes app" , "valtes" ) ?>
+                </button>
             </div>
         </div>
     </div>
 
     <script>
-        const mobileHeader = document.getElementById('mobile-header');
-        const menuToggle = document.getElementById('menuToggle');
-        const mobileMenu = document.getElementById('mobileMenu');
-        const bars = menuToggle.querySelectorAll('span');
+    const mobileHeader = document.getElementById('mobile-header');
+    const menuToggle = document.getElementById('menuToggle');
+    const mobileMenu = document.getElementById('mobileMenu');
+    const bars = menuToggle.querySelectorAll('span');
 
-        menuToggle.addEventListener('click', function () {
-            mobileMenu.classList.toggle('hidden');
-            if (!mobileMenu.classList.contains('hidden')) {
-                // Transform hamburger into close button
-                bars[0].classList.add('rotate-45', 'translate-y-[6px]');
-                bars[1].classList.add('opacity-0');
-                bars[2].classList.add('-rotate-45', '-translate-y-[6px]');
-                mobileHeader.classList.add('bg-white');
-            } else {
-                // Restore hamburger
-                bars[0].classList.remove('rotate-45', 'translate-y-[6px]');
-                bars[1].classList.remove('opacity-0');
-                bars[2].classList.remove('-rotate-45', '-translate-y-[6px]');
-                mobileHeader.classList.remove('bg-white');
-            }
-        });
+    menuToggle.addEventListener('click', function() {
+        mobileMenu.classList.toggle('hidden');
+        if (!mobileMenu.classList.contains('hidden')) {
+            // Transform hamburger into close button
+            bars[0].classList.add('rotate-45', 'translate-y-[6px]');
+            bars[1].classList.add('opacity-0');
+            bars[2].classList.add('-rotate-45', '-translate-y-[6px]');
+            mobileHeader.classList.add('bg-white');
+        } else {
+            // Restore hamburger
+            bars[0].classList.remove('rotate-45', 'translate-y-[6px]');
+            bars[1].classList.remove('opacity-0');
+            bars[2].classList.remove('-rotate-45', '-translate-y-[6px]');
+            mobileHeader.classList.remove('bg-white');
+        }
+    });
     </script>
 
 </header>
