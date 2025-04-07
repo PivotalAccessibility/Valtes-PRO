a3a91c8bce14c2e6d92540da59c0fe323b513d7a
Sanyam <ajay@pivotalaccessibility.com>
2025-04-07 10:32:41 +0530

    Bug fix for partners.

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 require_once get_template_directory() . '/lib/class-tgm-plugin-activation.php';
 
-define('PIVOTAL_ACCESSIBILITY_VERSION', '1.1.27');
+define('PIVOTAL_ACCESSIBILITY_VERSION', '1.1.28');
 
 add_action("after_setup_theme", "valtes_after_setup_theme");
 add_action("wp_enqueue_scripts", "valtes_enqueue_scripts");
--- template-parts/content_partners/partners.php
+++ template-parts/content_partners/partners.php
@@ -17,47 +17,52 @@ $partners = valtes_get_field('partners', []);
 
                 <?php foreach ($partners['all_partners'] as $partner): ?>
 
-                    <div class="flex flex-col items-center justify-between">
+                <div class="flex flex-col items-center justify-between">
 
-                        <div class=" px-10 flex flex-col items-center justify-center">
+                    <div class=" px-10 flex flex-col items-center justify-center">
 
-                            <?php if (!empty($partner['image']['url'])): ?>
-                                <div class="rounded-full h-44 w-44 bg-purple-400">
-                                    <img src="<?php echo $partner['image']['url'] ?>" alt="<?php echo $partner['image']['alt'] ?>" class="rounded-full h-44 w-44 object-cover">
-                                </div>
-                            <?php endif; ?>
-
-                            <?php if (!empty($partner['name'])): ?>
-                                <h2 class="font-bold text-center section-description mt-6">
-                                    <?php echo $partner['name']; ?>
-                                </h2>
-                            <?php endif; ?>
-
-                            <?php if (!empty($partner["company_logo"]['url'])): ?>
-                                <div class=" py-7">
-                                    <img src="<?php echo $partner["company_logo"]['url'] ?>" alt="<?php echo $partner["company_logo"]['alt'] ?>" class="h-auto w-1/2 justify-self-center">
-                                </div>
-                            <?php endif; ?>
-
-                            <?php if (!empty($partner['description'])): ?>
-                                <p class="section-sec-description text-center">
-                                    <?php echo wp_trim_words($partner['description'], 10, '...'); ?>
-                                </p>
-                            <?php endif; ?>
+                        <?php if (!empty($partner['image']['url'])): ?>
+                        <div class="rounded-full h-44 w-44 bg-purple-400">
+                            <img src="<?php echo $partner['image']['url'] ?>"
+                                alt="<?php echo $partner['image']['alt'] ?>"
+                                class="rounded-full h-44 w-44 object-cover">
                         </div>
+                        <?php endif; ?>
 
-                        <div class="mt-8">
-                            <?php if (!empty($partner["company_link"]['url'])): ?>
-                                <a href="<?php echo $partner["company_link"]['url'] ?>" class="btn btn-outline flex items-center">
-                                    <span class="mr-2">
-                                        <img src="<?php echo valtes_assets('images/up-right-from-squares.png') ?>" alt="">
-                                    </span>
-                                    <?php echo $partner["company_link"]['title'] ?>
-                                </a>
-                            <?php endif; ?>
+                        <?php if (!empty($partner['name'])): ?>
+                        <h2 class="font-bold text-center section-description mt-6">
+                            <?php echo $partner['name']; ?>
+                        </h2>
+                        <?php endif; ?>
+
+                        <?php if (!empty($partner["company_logo"]['url'])): ?>
+                        <div class=" py-7">
+                            <img src="<?php echo $partner["company_logo"]['url'] ?>"
+                                alt="<?php echo $partner["company_logo"]['alt'] ?>"
+                                class="h-auto w-1/2 justify-self-center">
                         </div>
+                        <?php endif; ?>
 
+                        <?php if (!empty($partner['description'])): ?>
+                        <p class="section-sec-description text-center">
+                            <?php echo $partner['description']; ?>
+                        </p>
+                        <?php endif; ?>
                     </div>
+
+                    <div class="mt-8">
+                        <?php if (!empty($partner["company_link"]['url'])): ?>
+                        <a href="<?php echo $partner["company_link"]['url'] ?>"
+                            class="btn btn-outline flex items-center">
+                            <span class="mr-2">
+                                <img src="<?php echo valtes_assets('images/up-right-from-squares.png') ?>" alt="">
+                            </span>
+                            <?php echo $partner["company_link"]['title'] ?>
+                        </a>
+                        <?php endif; ?>
+                    </div>
+
+                </div>
                 <?php endforeach; ?>
             </div>
         </div>
