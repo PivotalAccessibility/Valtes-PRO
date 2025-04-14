25ccd953076ce4a42880103d2ad09a3ac1a4b3df
Shadab Saifi (Office) <ajay@pivotalaccessibility.com>
2025-04-14 15:02:50 +0530

    Update single-post.php

--- single-post.php
+++ single-post.php
@@ -10,9 +10,9 @@
 $articles = valtes_get_field('articles', []);
 
 $args = array(
-    'post_type'      => 'post',
+    'post_type' => 'post',
     'posts_per_page' => 3,
-    'post_status'    => 'publish',
+    'post_status' => 'publish',
 );
 $query = new WP_Query($args);
 
@@ -25,37 +25,36 @@ get_header(); ?>
         <?php if (have_posts()):
             while (have_posts()):
                 the_post(); ?>
-        <div class="">
-            <?php if (has_post_thumbnail()): ?>
-            <div class="relative">
-                <div class="w-full mb-6">
-                    <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="<?php the_title(); ?>"
-                        class="test relative z-10 object-cover w-full rounded-full md:h-96 h-40">
-                </div>
-                <div class="md:h-20 md:w-20 h-9 w-9 rounded-full bg-[#2b37db] absolute top-2 -right-2"></div>
-                <div class="md:h-32 md:w-32 h-20 w-20 rounded-full bg-[#e1eaff] absolute bottom-0 left-0"></div>
-            </div>
-            <?php endif; ?>
-            <div class="flex flex-wrap md:flex-nowrap items-start sm:mt-20 mt-5 md:gap-32 gap-16">
-                <div class="sm:w-[70%] w-full h-auto">
-                    <p class="inline-block mb-4 text-base font-semibold text-primary">
-                        <?php $category = get_the_category(); ?>
-                        <?php echo $category ? esc_html($category[0]->name) : ''; ?>
-                    </p>
-                    
-                    <h1 class="mb-4 md:section-sec-heading section-heading">
-                        <?php the_title(); ?>
-                    </h1>
+                <div class="">
+                    <?php if (has_post_thumbnail()): ?>
+                        <div class="relative">
+                            <div class="w-full mb-6">
+                                <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" alt="<?php the_title(); ?>" class="test relative z-10 object-cover w-full rounded-full md:h-96 h-40">
+                            </div>
+                            <div class="md:h-20 md:w-20 h-9 w-9 rounded-full bg-[#2b37db] absolute top-2 -right-2"></div>
+                            <div class="md:h-32 md:w-32 h-20 w-20 rounded-full bg-[#e1eaff] absolute bottom-0 left-0"></div>
+                        </div>
+                    <?php endif; ?>
+                    <div class="flex flex-wrap md:flex-nowrap items-start sm:mt-20 mt-5 md:gap-32 gap-16">
+                        <div class="sm:w-[70%] w-full h-auto">
+                            <p class="inline-block mb-4 text-base font-semibold text-primary">
+                                <?php $category = get_the_category(); ?>
+                                <?php echo $category ? esc_html($category[0]->name) : ''; ?>
+                            </p>
 
-                    <div class="text-base text-black prose">
-                        <?php the_content(); ?>
+                            <h1 class="mb-4 md:section-sec-heading section-heading">
+                                <?php the_title(); ?>
+                            </h1>
+
+                            <div class="text-base text-black prose">
+                                <?php the_content(); ?>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
-                
-            </div>
-        </div>
-        <?php endwhile; endif; ?>
-        
+            <?php endwhile; endif; ?>
+
     </div>
 </section>
 
