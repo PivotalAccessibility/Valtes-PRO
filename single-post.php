8069485fa6ecc7e15602c9f29f3c416d214c4a3a
Sanyam <ajay@pivotalaccessibility.com>
2025-04-18 16:17:26 +0530

    Bug changes.

--- single-post.php
+++ single-post.php
@@ -102,7 +102,7 @@ get_header(); ?>
                         </span>
                         <div class="flex flex-col items-start mt-5 space-y-4 ">
 
-                            <button id="copyLinkButton" class="flex items-center text-sm text-blue-700">
+                            <button id="copyLinkButton" class="flex items-center text-sm text-blue-700 cursor-pointer">
                                 <span class="pr-3">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
